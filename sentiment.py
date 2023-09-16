6a7a8cedc599a3632579e85d3e93de216937114f
sahibakhan0310 <90564302+sahibakhan0310@users.noreply.github.com>
2023-09-16 13:17:18 -0500

    Add files via upload

---
+++ sentiment.py
@@ -0,0 +1,83 @@
+import os
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
+
+main_directory = "/Users/sahibakhan/Desktop/resources/ML/project/aclImdb/train"
+pos_rew = os.path.join(main_directory, "pos")
+neg_rew = os.path.join(main_directory, "neg")
+txt_files = [f for f in os.listdir(pos_rew) if f.endswith(".txt")]
+txt_files_neg = [f for f in os.listdir(neg_rew) if f.endswith(".txt")]
+
+# Create an empty list to store the text data
+data = []
+
+for txt_file in txt_files:
+    file_path = os.path.join(pos_rew, txt_file)
+    with open(file_path, "r") as file:
+        content = file.read().casefold()
+    # Append the content to the list
+    data.append(content)
+
+# Create a DataFrame from the list of text data
+df = pd.DataFrame(data, columns=["Text"])
+
+# Process the content of the DataFrame as needed
+# For example, you can add labels or perform further analysis
+df["Label"] = "positive"
+
+# Print the first few rows of the DataFrame to verify
+print(df.head())
+
+for txt_file in txt_files_neg:
+    file_path = os.path.join(neg_rew, txt_file)
+    with open(file_path, "r") as file:
+        content = file.read().casefold()
+    # Append the content to the list
+    data.append(content)
+
+# Create a DataFrame from the updated list of text data
+df = pd.DataFrame(data, columns=["Text"])
+
+# Process the content of the DataFrame as needed
+# For example, you can add labels or perform further analysis
+df["Label"] = "negative"
+
+# Print the first few rows of the DataFrame to verify
+print(df.head())
+
+
+#Tokenisation
+nltk.download('punkt')
+df['tokens'] = df['Text'].apply(nltk.word_tokenize)
+
+# Print the first few rows of the DataFrame to verify
+print(df.head())
+
+#stopwords
+nltk.download('stopwords')
+stopwords.words('english')
+
+def remove_stopwords(tokens):
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+    return filtered_tokens
+
+# Apply the remove_stopwords function to the 'tokens' column
+df['tokens'] = df['tokens'].apply(remove_stopwords)
+
+# Print the first few rows of the DataFrame to verify
+print(df.head())
+
+#df['tokens']=stemmer.stem(df['tokens'])
+#print(df.head())
+
+def stemming_words(tokens):
+    new_tokens=[stemmer.stem(token) for token in tokens]
+    return new_tokens
+
+df['tokens']=df['tokens'].apply(stemming_words)
+print(df.head())
