3a7c6d8f50df001c688c1afd24051015fba8e8d8
tomaraakansha <akanksha.official1@gmail.com>
2023-11-30 12:29:11 -0600

    changes

--- new_sentiment.py
+++ new_sentiment.py
@@ -12,6 +12,9 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from gensim.scripts.glove2word2vec import glove2word2vec
 from gensim.models import KeyedVectors
+from random import seed  # Set seed for reproducibility
+
+seed(42)  # Set seed for reproducibility
 
 # Download and load pre-trained GloVe embeddings
 glove_file = './glove.6B.300d.txt'
@@ -20,53 +23,55 @@ glove2word2vec(glove_file, word2vec_output_file)
 word2Vec_model = KeyedVectors.load_word2vec_format(word2vec_output_file, binary=False)
 
 def load_data(main_directory):
+    # Load positive reviews
     pos_rew = os.path.join(main_directory, "pos")
-    neg_rew = os.path.join(main_directory, "neg")
-
-    data = []
-
+    data_pos = []
     for txt_file in os.listdir(pos_rew):
         file_path = os.path.join(pos_rew, txt_file)
         with open(file_path, "r", encoding='utf-8') as file:
             content = file.read().casefold()
-        data.append(content)
+        data_pos.append(content)
 
-    df_pos = pd.DataFrame(data, columns=["Text"])
-    df_pos["Label"] = "positive"
-
-    data = []
-
+    # Load negative reviews
+    neg_rew = os.path.join(main_directory, "neg")
+    data_neg = []
     for txt_file in os.listdir(neg_rew):
         file_path = os.path.join(neg_rew, txt_file)
         with open(file_path, "r", encoding='utf-8') as file:
             content = file.read().casefold()
-        data.append(content)
+        data_neg.append(content)
 
-    df_neg = pd.DataFrame(data, columns=["Text"])
+    # Create DataFrames
+    df_pos = pd.DataFrame(data_pos, columns=["Text"])
+    df_pos["Label"] = "positive"
+
+    df_neg = pd.DataFrame(data_neg, columns=["Text"])
     df_neg["Label"] = "negative"
 
+    # Concatenate positive and negative DataFrames
     df = pd.concat([df_pos, df_neg])
 
     return df
 
 def preprocess_data(df):
+    # Tokenization and stemming
     stop_words = set(stopwords.words('english'))
     stemmer = PorterStemmer()
-
     df['tokens'] = df['Text'].apply(lambda text: [stemmer.stem(word) for word in text.split() if word.lower() not in stop_words])
 
+    # Padding tokens to a fixed length
     max_length = df['tokens'].apply(len).max()
     padding_token = '<PAD>'
-
     df['tokens'] = df['tokens'].apply(lambda tokens: tokens + [padding_token] * (max_length - len(tokens)))
 
+    # Save the preprocessed data to a CSV file
     df.to_csv('padded_tokens.csv', index=False)
     return df
 
 def convert_to_word_embeddings(df, max_sequence_length):
+    # Convert tokens to word embeddings using GloVe
     review_vectors = []
     labels = []
-
     for i, tokens in enumerate(df['tokens']):
         vectors = [word2Vec_model[word] for word in tokens if word in word2Vec_model]
         if vectors:
@@ -77,7 +82,6 @@ def convert_to_word_embeddings(df, max_sequence_length):
         label = int(df['Label'].iloc[i] == 'positive')
         labels.append(label)
 
-    
     # Convert to numpy arrays
     review_vectors = np.array(review_vectors)
     labels = np.array(labels)
@@ -87,35 +91,40 @@ def convert_to_word_embeddings(df, max_sequence_length):
     review_vectors = review_vectors[not_nan_mask]
     labels = labels[not_nan_mask]
 
+    # Pad sequences to a fixed length
     padded_word_vectors = pad_sequences(review_vectors, maxlen=max_sequence_length, padding='post', dtype='float32')
-    print(f"Shape of review_vectors: {len(review_vectors)}")
-    print(f"Shape of labels: {len(labels)}")
+
     return padded_word_vectors, labels
 
-
 def split_data(padded_word_vectors, labels):
+    # Split data into training and validation sets
     X_train, X_val, y_train, y_val = train_test_split(padded_word_vectors, labels, test_size=0.2, random_state=42)
     X_train = np.expand_dims(X_train, axis=1)
     X_val = np.expand_dims(X_val, axis=1)
     return X_train, X_val, y_train, y_val
 
 def build_lstm_model(input_shape):
+    # Build LSTM model
     model = Sequential()
-    model.add(Bidirectional(LSTM(200, return_sequences=True), input_shape=input_shape))
+    model.add(Bidirectional(LSTM(100, return_sequences=True), input_shape=input_shape))
     model.add(GlobalAveragePooling1D())
     model.add(Dropout(0.2))
     model.add(Dense(1, activation='sigmoid'))
     custom_optimizer = Adam(learning_rate=0.001)
     model.compile(loss='binary_crossentropy', optimizer=custom_optimizer, metrics=['accuracy'])
     model.summary()
+
+    # Save the model
     model.save('lstm_model.h5')
     return model
 
-def train_model(model, X_train, y_train, X_val, y_val, epochs=20, batch_size=64):
+def train_model(model, X_train, y_train, X_val, y_val, epochs=15, batch_size=64):
+    # Train the model
     history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_val, y_val))
     return history
 
 def evaluate_model(model, X_val, y_val):
+    # Evaluate the model on validation data
     y_pred = model.predict(X_val)
     y_pred = np.round(y_pred)
     accuracy = accuracy_score(y_val, y_pred)
@@ -124,29 +133,6 @@ def evaluate_model(model, X_val, y_val):
 
     print("Accuracy: ", accuracy)
     print("Classification Report:\n", classification_rep)
-    print("Confusion Matrix:\n", confusion_mat)
-
-
-# Main execution
-main_directory = "./aclImdb/train"
-df = load_data(main_directory)
-df = preprocess_data(df)
-max_sequence_length = df['tokens'].apply(len).max()
-padded_word_vectors, labels = convert_to_word_embeddings(df, max_sequence_length)
-
-X_train, X_val, y_train, y_val = split_data(padded_word_vectors, labels)
-
-input_shape = X_train.shape[1:]
-
-model = build_lstm_model(input_shape)
-history = train_model(model, X_train, y_train, X_val, y_val)
-'''
-
-from keras.models import load_model
-# Load the saved model
-model = load_model('lstm_model.h5')
-'''
-evaluate_model(model, X_val, y_val)
 
 def load_test_data(test_directory):
     pos_test_dir = os.path.join(test_directory, "pos")
@@ -174,14 +160,32 @@ def load_test_data(test_directory):
     df_test = pd.DataFrame({'Text': data, 'Label': labels})
     return df_test
 
-def classify_test_data(model, df_test,  max_sequence_length):
+def classify_test_data(model, df_test, max_sequence_length):
     X_test, y_test = convert_to_word_embeddings(df_test, max_sequence_length)
-    X_test = np.expand_dims(X_test, axis=1)  # Add this line
+    X_test = np.expand_dims(X_test, axis=1)
 
     y_pred = model.predict(X_test)
     y_pred = np.round(y_pred)
 
     return y_test, y_pred
+
+
+# Main execution
+main_directory = "./aclImdb/train"
+df = load_data(main_directory)
+df = preprocess_data(df)
+max_sequence_length = df['tokens'].apply(len).max()
+padded_word_vectors, labels = convert_to_word_embeddings(df, max_sequence_length)
+
+X_train, X_val, y_train, y_val = split_data(padded_word_vectors, labels)
+
+input_shape = X_train.shape[1:]
+
+model = build_lstm_model(input_shape)
+history = train_model(model, X_train, y_train, X_val, y_val)
+
+# Evaluate the model on validation data
+evaluate_model(model, X_val, y_val)
 
 # Load and preprocess the test data
 test_directory = "./aclImdb/test"
@@ -196,5 +200,4 @@ y_test, y_pred_test = classify_test_data(model, df_test,  max_sequence_length)
 
 print('Test data classification results:#########')
 print(f'Classification Report:\n {classification_report(y_test, y_pred_test)}')
-print(f'Confusion Matrix:\n {confusion_matrix(y_test, y_pred_test)}')
 print(f'Accuracy: {accuracy_score(y_test, y_pred_test)}')
